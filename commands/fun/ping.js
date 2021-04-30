b9b3c9424feede2442a19e3519a678994e9268cd
Benni <73313922+BenjaminBruenau@users.noreply.github.com>
2021-05-01 00:33:10 +0200

    Added some first test commands

--- commands/fun/ping.js
+++ commands/fun/ping.js
@@ -1,6 +1,7 @@
 module.exports = {
     name: 'ping',
     description: 'Ping!',
+    disabled: 'true',
     execute(message, args) {
         message.channel.send('Pong!');
     },
--- index.js
+++ index.js
@@ -5,11 +5,15 @@ const { prefix, token} = require('./config.json');
 const client = new Discord.Client();
 
 client.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFolders = fs.readdirSync('./commands');
 
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.name, command);
+for (const folder of commandFolders) {
+    const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+
+    for (const file of commandFiles) {
+        const command = require(`./commands/${folder}/${file}`);
+        client.commands.set(command.name, command);
+    }
 }
 
 client.once('ready', () => {
@@ -22,15 +26,29 @@ client.on('message', message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
     const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const command = args.shift().toLowerCase()
+    const commandName = args.shift().toLowerCase();
     /*
     if (message.content === `${prefix}server`) {
         message.channel.send(`This server's name is: ${message.guild.name} \nMembers: ${message.guild.memberCount}`);
      */
-    if (!client.commands.has(command)) return;
+    if (!client.commands.has(commandName)) return;
+
+    const command = client.commands.get(commandName);
+    if (command.disabled) {
+        return console.log(`${prefix}${commandName} is disabled!`);
+    }
+
+    if (command.args && !args.length) {
+        let reply = `You did not provide any arguments, ${message.author}!`;
+
+        if (command.usage) {
+            reply += `\nThe correct usage is: \`${prefix}${commandName} ${command.usage}\``;
+        }
+        return message.channel.send(reply);
+    }
 
     try {
-        client.commands.get(command).execute(message, args);
+        command.execute(message, args);
     } catch (error) {
         console.error(error);
         message.reply('There was an error trying to execute this command!');
