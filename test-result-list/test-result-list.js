1e6728ccd9a9c6ae2d510fc2939befcc5de69826
Benni <73313922+BenjaminBruenau@users.noreply.github.com>
2021-05-07 19:51:52 +0200

    Add Ascii Art List

--- test-result-list/test-result-list.js
+++ test-result-list/test-result-list.js
@@ -16,16 +16,18 @@ let resultList = {
     array: results
 };
 
-exports.addTestResult = function (user, message) {
+exports.addTestResult = async function (user, message) {
     //ToDo: Maybe find a way to only call this after a restart
-    initResultArray();
+    await initResultArray();
     const start = new Date();
     const expirationDate = new Date();
     expirationDate.setDate(start.getDate() + 4);
-    let roomNumber = 'tmp';
-    message.guild.members.fetch(user.id)
+    let roomNumber = '';
+    await message.guild.members.fetch(user.id)
         .then(member => {
-            roomNumber = member.roles.cache.find(role => role.name.startsWith('W'));
+            let role = member.roles.cache.find(role => role.name.startsWith('W'));
+            roomNumber = role.name;
+            console.log(`${roomNumber}`);
         })
         .catch(error => {
             console.error('Could not fetch guild member!', error);
@@ -36,7 +38,7 @@ exports.addTestResult = function (user, message) {
         name: user.username,
         roomNr: roomNumber,
         emoji: config.confirm_test,
-        expiration: dateformat(expirationDate, 'dd/mm/yy' )
+        expiration: dateformat(expirationDate, 'dd/mm/yy')
     }
     const duplicate = results.filter(element => element.roomNr === entry.roomNr)
     // If there is already an entry from that person => replace it
@@ -49,17 +51,19 @@ exports.addTestResult = function (user, message) {
     }
 
     results.push(entry);
-    updateResultList();
+    await updateResultList();
     const resultsString = JSON.stringify(resultList, null, 4);
     //console.log(resultsString);
     //ToDo: Move the file? If yes how
-    fs.writeFile("test-result-list.json", resultsString, (error) => {
+    await fs.writeFile("test-result-list.json", resultsString, (error) => {
         if (error) {
             console.error('Error while saving to json File', error);
             return console.log(resultsString);
         }
     });
-    updateList(message.client).then(() => { console.log('Finished Adding Test Result') });
+    updateList(message.client).then(() => {
+        console.log('Finished Adding Test Result')
+    });
 }
 
 
@@ -70,30 +74,59 @@ async function updateList(client) {
     if (!listMessage) {
         await deleteAllMessages(listChannel);
         const initEmbed = new Discord.MessageEmbed().setDescription('Initiating List');
-        await listChannel.send(initEmbed).then(table => {
-            config.listMessage_ID = table.id;
-            listMessage = table;
-            // Save new ID to config
-            const configString = JSON.stringify(config, null, 4);
-            fs.writeFile('../config.json', configString, (error) => {
-                if (error) {
-                    console.error('Error while saving to json File', error);
-                    return console.log(configString);
-                }
+        await listChannel.send(initEmbed)
+            .then(table => {
+
+                listMessage = table;
+                console.log(table.id);
+                console.log('Send init Message')
+            }).catch(error => {
+                console.error('Error while sending new Test-Result List', error);
             });
-        }).catch(error => {
-            console.error('Error while sending new Test-Result List', error);
+        // Save new ID to config
+        config.listMessage_ID = listMessage.id;
+        const configString = JSON.stringify(config, null, 2);
+        //ToDo: Something doesnt work here
+        await fs.writeFile('../config.json', configString, (error) => {
+            if (error) {
+                console.error('Error while saving to json File', error);
+                return console.log(configString);
+            }
+            //console.log(configString);
+            console.log('Saved Config');
         });
     }
-    const embedTable = new Discord.MessageEmbed().setDescription('bye');
-    await listMessage.edit(embedTable);
+
+    await buildList(listMessage)
     //ToDo: Build List String (Ascii Art, maybe html/css)
     console.log('Updated Test-Result List');
 }
 
-function deleteAllMessages(channel) {
+async function buildList(listMessage) {
+    let list = '**Name**'.padEnd(12, '\u3000') + '**Room**'.padEnd(5, '\u3000') + '\u3000\u3000\u3000**Expiring**\n';
+    const split = '+---------------+------+-----+-----------+\n';
+
+    results.forEach(entry => {
+        list += split + `${entry.name}`.padEnd(14 - entry.name.length,'\u3000') + `${entry.roomNr} `
+            + `\u3000${entry.emoji}\u3000${entry.expiration}\n`;
+    });
+    const embedTable = new Discord.MessageEmbed()
+        .setTitle('__**Test Results**__')
+        .setDescription(list)
+        .setFooter('|' + '\u3000'.repeat(20) +  '|');
+    await listMessage.edit(embedTable)
+        .then(result => {
+            console.log('Successfully Edited List');
+        })
+        .catch(error => {
+            console.error('Error while editing List', error);
+        });
+
+}
+
+async function deleteAllMessages(channel) {
     // Can only delete last 100
-    channel.messages.fetch({limit:99})
+    await channel.messages.fetch({limit:99})
         .then(messages => {
             channel.bulkDelete(messages)
                 .then(() => {
